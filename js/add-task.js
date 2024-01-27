a20228acf50d7a31c50ada6f86d044a16ff96726
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-01-27 15:01:10 +0100

    added add task

--- js/add-task.js
+++ js/add-task.js
@@ -8,18 +8,18 @@ function renderAddTask() {
             <form>
                 <div class="">
                     <label for="" class="">Title<p class="redstar">*</p></label>
-                    <input type="text" class="" id="" placeholder="Enter a title" required>
+                    <input type="text" class="inputfield" id="" placeholder="Enter a title" required>
                     <div id="" class="red">
                         This field is required
                     </div>
                 </div>
                 <div class="">
                     <label class="">Description</label>
-                    <textarea class="" placeholder="Enter a Description"></textarea>
+                    <textarea class="inputfield" placeholder="Enter a Description"></textarea>
                 </div>
                 <label>Assigned to</label>
-                <select class="">
-                    <option selected>Select contacts to assign</option>
+                <select class="inputfield" aria-placeholder="">
+                    <option value="" disabled selected hidden>Select contacts to assign</option>
                     <option value="">Anton Mayer</option>
                     <option value="">Anja Schulz</option>
                     <option value="">Sofia Müller</option>
@@ -30,7 +30,7 @@ function renderAddTask() {
                 <div class="">
                     <label for="" class="">Due date<p class="redstar">*</p></label>
                     <div>
-                        <input type="text" class="" id="" placeholder="dd/mm/yyyy" required>
+                        <input type="text" class="inputfield" id="" placeholder="dd/mm/yyyy" required>
                         <img src="/assets/img/icons/calender.svg" alt="Calender">
                     </div>
                     <div id="" class="d-none">
@@ -38,20 +38,27 @@ function renderAddTask() {
                     </div>
                 </div>
                 <label for="">Prio</label>
-                <div class="" role="group">
+                <div class="prio-btn" role="group">
                     <button type="button" class="">Urgent <img src="/assets/img/icons/prio-urgent.svg" alt="Urgent Prio"></button>
                     <button type="button" class="">Medium <img src="/assets/img/icons/prio-medium.svg" alt="Medium Prio"></button>
                     <button type="button" class="">Low <img src="/assets/img/icons/prio-low.svg" alt="Low Prio"></button>
                 </div>
                 <label>Category<p class="redstar">*</p></label>
-                <select class="">
+                <select class="inputfield">
                     <option selected>Select task category</option>
                     <option value="">Technical Task</option>
                     <option value="">User Story</option>
                 </select>
                 <label for="">Subtasks</label>
-                <input type="text">
+                <input type="text" class="inputfield">
             </form>
+            <div>
+                <p><p class="red">*</p>This field is required</p>
+                <div>
+                    <button>Clear<img src="" alt="Clear"></button>
+                    <button>Create Task<img src="" alt="Create Task"></button>
+                </div>
+            </div>
         </div>    
     `;
 }
