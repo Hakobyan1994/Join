8620cebf4b1b943988adff76ed174e26d8acd4a8
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-02-28 21:06:26 +0100

    remember Me function

--- js/register.js
+++ js/register.js
@@ -1,12 +1,12 @@
-let userGuests=[
+let userGuests = [
     {
-       name:'',
-       greeting:'Good Morning'
+        name: '',
+        greeting: 'Good Morning'
     },
 ]
-   console.log(userGuests[0].greeting);
+console.log(userGuests[0].greeting);
 
-let div =  document.getElementById('animationDiv')
+let div = document.getElementById('animationDiv')
 let img = document.getElementById('logo-login')
 let stop = JSON.parse(localStorage.getItem('stop'))
 if (stop) {
@@ -19,7 +19,7 @@ if (stop) {
     img.classList.add('join_image')
 }
 
-setTimeout(()=> {
+setTimeout(() => {
     div.classList.remove('startParDiv')
     div.classList.add('anim_div')
     img.classList.remove('startParImg')
@@ -37,11 +37,30 @@ console.log(dataUser);
 
 let form_log = document.getElementById('form_log');
 let checkBox = document.getElementById('checkBox');
+let emailInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
 
-checkBox.checked
- 
-form_log.onsubmit = validLogin;
+
+    function showUserdata(){
+        if(checkBox.checked){
+            if(dataUser.length>0){
+                const user=dataUser[0];
+                emailInput.value=user.email;
+                passwordInput.value=user.password
+            }
+        }else{
+            emailInput.value = '';
+            passwordInput.value = '';
+        }
+    }
     
+
+  
+    checkBox.addEventListener('change',showUserdata)
+
+
+    form_log.onsubmit = validLogin;
+
 
 function validLogin(e) {
     e.preventDefault();
@@ -53,6 +72,8 @@ function validLogin(e) {
         if (foundUser) {
             if (foundUser.password === password) {
                 status = 'ok';
+               
+
             } else {
                 status = 'Error Password';
             }
@@ -66,7 +87,7 @@ function validLogin(e) {
             checkBox.checked = false;
             document.getElementById('email').style.border = `1px solid red`;
             document.getElementById('password').style.border = `1px solid red`;
-            document.querySelector('#errorMessage').innerText='The password or Email is not correct'
+            document.querySelector('#errorMessage').innerText = 'The password or Email is not correct'
             document.getElementById('imageInput').classList.add('passwordImageError');
 
         }
@@ -74,44 +95,44 @@ function validLogin(e) {
 }
 
 
-document.addEventListener('DOMContentLoaded',function(){
- const passwordInput=document.getElementById('password')
- const inputImage=document.getElementById('imageInput')
-  passwordInput.addEventListener('input',function(){
-   if(passwordInput.value!==''){
-    inputImage.src='../assets/img/anmeldung Image/blockPassword.svg'
-    inputImage.onclick=function(){
-       if(passwordInput.type==='password'){
-         passwordInput.type='text'
-         inputImage.src='../assets/img/anmeldung Image/passwordLock.svg'
-       }else{
-          passwordInput.type='password'
-          inputImage.src='../assets/img/anmeldung Image/blockPassword.svg'
-       }
-    }
-   }else{
-     inputImage.src='../assets/img/anmeldung Image/lock.png'
-   }
-  })    
-})   
-  
-       
-setTimeout(()=> {
+document.addEventListener('DOMContentLoaded', function () {
+    const passwordInput = document.getElementById('password')
+    const inputImage = document.getElementById('imageInput')
+    passwordInput.addEventListener('input', function () {
+        if (passwordInput.value !== '') {
+            inputImage.src = '../assets/img/anmeldung Image/blockPassword.svg'
+            inputImage.onclick = function () {
+                if (passwordInput.type === 'password') {
+                    passwordInput.type = 'text'
+                    inputImage.src = '../assets/img/anmeldung Image/passwordLock.svg'
+                } else {
+                    passwordInput.type = 'password'
+                    inputImage.src = '../assets/img/anmeldung Image/blockPassword.svg'
+                }
+            }
+        } else {
+            inputImage.src = '../assets/img/anmeldung Image/lock.png'
+        }
+    })
+})
+
+
+setTimeout(() => {
     localStorage.setItem('stop', JSON.stringify('stop'))
-},3000)
+}, 3000)
 
 
-function activUser(arr){
-    localStorage.setItem('activeUser',JSON.stringify(arr));
- }  
+function activUser(arr) {
+    localStorage.setItem('activeUser', JSON.stringify(arr));
+}
 
- function guesButton(){ 
+function guesButton() {
     localStorage.removeItem('activeUser');
-    forGuestUser('guestsUser',userGuests);
-    window.location.href='../files/summary.html'
- }
-      
+    forGuestUser('guestsUser', userGuests);
+    window.location.href = '../files/summary.html'
+}
 
-     function forGuestUser(key,arr){
-        localStorage.setItem(key,JSON.stringify(arr))
-     }
+
+function forGuestUser(key, arr) {
+    localStorage.setItem(key, JSON.stringify(arr))
+}
